22243bb634ba735f9f1675357350352a0f3277e0
jfdaza <mainekko23@gmail.com>
2024-01-22 16:54:44 -0300

    sidebar completo

--- src/components/SideBar/SideBarMenuComponent/SideBarMenuComponent.jsx
+++ src/components/SideBar/SideBarMenuComponent/SideBarMenuComponent.jsx
@@ -1,44 +1,47 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import "./SideBarMenuComponent.css";
-import Toggle from "../../../assets/img/toggles/arrow-down.svg";
-import 'animate.css';
+import "animate.css";
+import { MenuComponent } from "./MenuComponent/MenuComponent";
 
 export const SideBarMenuComponent = () => {
-  const [isBancosExpanded, setIsBancosExpanded] = useState(false);
-  const [isConfiguracionExpanded, setIsConfiguracionExpanded] = useState(false);
-
-  const toggleBancos = () => {
-    setIsBancosExpanded(!isBancosExpanded);
+  const menuNames = {
+    BANCOS: 'Bancos',
+    CONFIGURACION: 'Configuracion'
   };
 
-  const toggleConfiguracion = () => {
-    setIsConfiguracionExpanded(!isConfiguracionExpanded);
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const toggleMenu = (menuName) => {
+    setActiveMenu(activeMenu === menuName ? null : menuName);
+  };
+
+  const links = {
+    [menuNames.BANCOS]: [
+      { path: "/banco1", name: "Banco 1" },
+      { path: "/banco2", name: "Banco 2" }
+    ],
+    [menuNames.CONFIGURACION]: [
+      { path: "/config1", name: "Config 1" },
+      { path: "/config2", name: "Config 2" }
+    ]
   };
 
   return (
     <div className="sidebar-menu">
       <ul>
-        <li onClick={toggleBancos}><div className="option-menu" ><span>Bancos</span> <span><img className={isBancosExpanded ? 'rotate-toggle' : ''} src={Toggle} alt="dropdown toggle" /></span></div></li>
-        {isBancosExpanded && (
-          <ul className={`submenu ${isBancosExpanded ? 'animate__animated animate__slideInUp  expanded' : 'contracted'}`}>
-            <li><Link to="/banco1">Banco 1</Link></li>
-            <li><Link to="/banco2">Banco 2</Link></li>
-          </ul>
-        )}
-        <li onClick={toggleConfiguracion}><div className="option-menu" ><span>Configuracion</span> <span><img className={isConfiguracionExpanded ? 'rotate-toggle' : ''} src={Toggle} alt="dropdown toggle" /></span></div></li>
-        {isConfiguracionExpanded && (
-          <ul className={`submenu ${isConfiguracionExpanded ? 'animate__animated animate__slideInUp expanded' : 'contracted'}`}>
-            <li><Link to="/banco1">Banco 1</Link></li>
-            <li><Link to="/banco2">Banco 2</Link></li>
-          </ul>
-        )}
+        <MenuComponent
+          menuName={menuNames.BANCOS}
+          isActive={activeMenu === menuNames.BANCOS}
+          toggleMenu={toggleMenu}
+          links={links[menuNames.BANCOS]}
+        />
+        <MenuComponent
+          menuName={menuNames.CONFIGURACION}
+          isActive={activeMenu === menuNames.CONFIGURACION}
+          toggleMenu={toggleMenu}
+          links={links[menuNames.CONFIGURACION]}
+        />
       </ul>
     </div>
   );
 };
-
-
-// 
-//       <Link to="/dashboard">Dashboard</Link>
-//       <Link to="/about">About</Link>
