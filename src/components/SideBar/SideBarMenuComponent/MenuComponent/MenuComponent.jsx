96335b7237f25294de6591fcaac8cb67b8bfc416
jfdaza <mainekko23@gmail.com>
2024-01-25 10:14:59 -0300

    Refactor MenuComponent to include SubmenuComponent

--- src/components/SideBar/SideBarMenuComponent/MenuComponent/MenuComponent.jsx
+++ src/components/SideBar/SideBarMenuComponent/MenuComponent/MenuComponent.jsx
@@ -1,41 +1,35 @@
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Toggle from "../../../..//assets/img/toggles/arrow-down.svg";
+import { SubmenuComponent } from "./SubmenuComponent/SubmenuComponent";
 
 export const MenuComponent = ({ menuName, isActive, toggleMenu, links }) => {
-    return (
-        <>
-            <li onClick={() => toggleMenu(menuName)}>
-                <div className="option-menu">
-                    <span>{menuName}</span>{" "}
-                    <span>
-                        <img
-                            className={isActive ? "rotate-toggle" : ""}
-                            src={Toggle}
-                            alt="dropdown toggle"
-                        />
-                    </span>
-                </div>
-            </li>
-            <ul className={`submenu ${isActive ? 'isActive' : ''}`}>
-                {links.map((link, index) => (
-                    <li key={index}>
-                        <Link to={link.path}>{link.name}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
-}
+  return (
+    <>
+      <li onClick={() => toggleMenu(menuName)}>
+        <div className="option-menu">
+          <span>{menuName}</span>{" "}
+          <span>
+            <img
+              className={isActive ? "rotate-toggle" : ""}
+              src={Toggle}
+              alt="dropdown toggle"
+            />
+          </span>
+        </div>
+      </li>
+      <SubmenuComponent isActive={isActive} links={links} />
+    </>
+  );
+};
 
 MenuComponent.propTypes = {
-    menuName: PropTypes.string.isRequired,
-    isActive: PropTypes.bool.isRequired,
-    toggleMenu: PropTypes.func.isRequired,
-    links: PropTypes.arrayOf(
-        PropTypes.shape({
-            path: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired
-        })
-    ).isRequired
+  menuName: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  toggleMenu: PropTypes.func.isRequired,
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  ).isRequired
 };
---
+++ src/components/SideBar/SideBarMenuComponent/MenuComponent/SubmenuComponent/SubmenuComponent.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export const SubmenuComponent = ({ isActive, links }) => {
+  return (
+    <ul className={`submenu ${isActive ? 'isActive' : ''}`}>
+      {links.map((link, index) => (
+        <li key={index}>
+          <Link to={link.path}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+SubmenuComponent.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
