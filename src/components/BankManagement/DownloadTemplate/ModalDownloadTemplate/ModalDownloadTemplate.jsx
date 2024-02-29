c268b636dab5a5403285ec796f4dc0e9cbee14c4
jfdaza <mainekko23@gmail.com>
2024-02-29 10:03:28 -0300

    Actualizar nombre de archivo de descarga en ModalDownloadTemplate.jsx

--- src/components/BankManagement/DownloadTemplate/ModalDownloadTemplate/ModalDownloadTemplate.jsx
+++ src/components/BankManagement/DownloadTemplate/ModalDownloadTemplate/ModalDownloadTemplate.jsx
@@ -36,7 +36,7 @@ export const DownloadTemplateModal = ({ show, handleClose, bankName }) => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", `Conciliaciones-${bankName}.xlsx`);
+      link.setAttribute("download", `Conciliaciones-${bankName}-${formattedDate}.xlsx`);
       document.body.appendChild(link);
       link.click();
       setTimeout(() => {
