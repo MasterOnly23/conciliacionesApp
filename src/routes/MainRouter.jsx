345c2fa582fe733c109e245ef483e9dd1778c362
jfdaza <mainekko23@gmail.com>
2024-04-25 17:28:03 -0300

    Actualizar rutas y dependencias de paquetes

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,32 +1,46 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy } from "react";
 import SideBarMainComponent from "../components/SideBar/SideBarMainComponent/SideBarMainComponent";
-import { Header } from "../components/Header/header";
+import { Header } from "../components/Header/Header";
 import { Loadder } from "../components/Loadder/Loadder";
 
-const DashBoard = lazy(() => import("../components/DashBoard/DashBoard").then(module => ({ default: module.DashBoard })));
-const BankManagement = lazy(() => import("../components/BankManagement/BankManagement").then(module => ({ default: module.BankManagement })));
-const Historial = lazy(() => import("../components/Historial/table/HistorialListComponent").then(module => ({ default: module.HistorialListComponent })));
-const Page404 = lazy(() => import("../components/Page404/Page404").then(module => ({ default: module.Page404 })));
-
+const DashBoard = lazy(() =>
+  import("../components/DashBoard/DashBoard").then((module) => ({
+    default: module.DashBoard,
+  }))
+);
+const BankManagement = lazy(() =>
+  import("../components/BankManagement/BankManagement").then((module) => ({
+    default: module.BankManagement,
+  }))
+);
+const Historial = lazy(() =>
+  import("../components/Historial/table/HistorialListComponent").then(
+    (module) => ({ default: module.HistorialListComponent })
+  )
+);
+const Page404 = lazy(() =>
+  import("../components/Page404/Page404").then((module) => ({
+    default: module.Page404,
+  }))
+);
 
 const MainRoutes = () => {
   return (
     <Router>
-        <div className="sidebarRoute">
-      <SideBarMainComponent />
-      <div className="contentRoutes">
-      <Header />
-      <Suspense fallback={<Loadder/>}>
-      <Routes>
-      <Route path="/" exact element={<DashBoard />} />
-        <Route path="/historial" element={<Historial/>} />
-        <Route path="/:Banco" element={<BankManagement/>} />
-        <Route path="/*" element={<Page404 />} />
-      </Routes>
-      </Suspense>
-      
-      </div>
+      <div className="sidebarRoute">
+        <SideBarMainComponent />
+        <div className="contentRoutes">
+          <Header />
+          <Suspense fallback={<Loadder />}>
+            <Routes>
+              <Route path="/" exact element={<DashBoard />} />
+              <Route path="/historial" element={<Historial />} />
+              <Route path="/:Banco" element={<BankManagement />} />
+              <Route path="/*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
+        </div>
       </div>
     </Router>
   );
